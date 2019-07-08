402681a316188176c99e288cdea271f6ddb96052
burawi <burawi.ba@gmail.com>
2019-07-08 13:49:01 +0100

    delete extractor

--- iconExtractor.js
+++
@@ -1,6 +0,0 @@
-import * as icons from "/node_modules/@mdi/js/mdi.js";
-
-export function getPath(iconName) {
-  let capitalized = iconName.charAt(0).toUpperCase() + iconName.slice(1);
-  return icons[`mdi${capitalized}`]
-}
